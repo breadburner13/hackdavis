6bc3ed4a852202f8d40ea1ea71a3a1b2888fb089
BreadBurner13 <jungkrischan@gmail.com>
2021-01-16 20:37:04 -0800

    add images, results

--- results.js
+++ results.js
@@ -12,3 +12,265 @@ function displayInput() {
         document.body.appendChild(text); // Appends the text created above to results.html 
     }
 }
+
+var testNames = ["HBA1C", "CBC", "CMP", "Lipid Panel", "Prostate Surface Antigen (PSA)", "CA19", "Sexually", "Urine Toxicology", "Urinalysis", "C-reactive Protein", "Liver Panel", "D Dimer", "Influenza Panel", "COVID"]
+
+function HBA1C(value){
+var health;  
+var symptoms;
+    if (value < 5.7) {
+        health = "Normal";
+        symptoms = 
+
+    }
+    else if (value > 5.7 && value < 6.4){
+        health = "Prediabetes";
+        symptoms = "Less severe symptoms of diabetes, which include frequent thirst, urination, blurry vision, and constant fatigue"
+
+    }
+    else {
+        health = Diabetes;
+        symptoms = "Frequent Thirst, Urination, Blurry Vision, Constant Fatigue"
+    }
+}
+function CBC(value){
+    var health;  
+    var symptoms;
+        if (value < 5.7) {
+            health = "Normal";
+            symptoms = 
+    
+        }
+        else if (value > 5.7 && value < 6.4){
+            health = "Prediabetes";
+            symptoms = "Less severe symptoms of diabetes, which include frequent thirst, urination, blurry vision, and constant fatigue"
+    
+        }
+        else {
+            health = Diabetes;
+            symptoms = "Frequent Thirst, Urination, Blurry Vision, Constant Fatigue"
+        }
+    }
+function CMP(value){
+var health;  
+var symptoms;
+    if (value < 5.7) {
+        health = "Normal";
+        symptoms = 
+
+    }
+    else if (value > 5.7 && value < 6.4){
+        health = "Prediabetes";
+        symptoms = "Less severe symptoms of diabetes, which include frequent thirst, urination, blurry vision, and constant fatigue"
+
+    }
+    else {
+        health = Diabetes;
+        symptoms = "Frequent Thirst, Urination, Blurry Vision, Constant Fatigue"
+    }
+}
+function lipidPanel(value){
+    var health;  
+    var symptoms;
+        if (value < 5.7) {
+            health = "Normal";
+            symptoms = 
+    
+        }
+        else if (value > 5.7 && value < 6.4){
+            health = "Prediabetes";
+            symptoms = "Less severe symptoms of diabetes, which include frequent thirst, urination, blurry vision, and constant fatigue"
+    
+        }
+        else {
+            health = Diabetes;
+            symptoms = "Frequent Thirst, Urination, Blurry Vision, Constant Fatigue"
+        }
+}
+function HBA1C(value){
+    var health;  
+    var symptoms;
+        if (value < 5.7) {
+            health = "Normal";
+            symptoms = 
+    
+        }
+        else if (value > 5.7 && value < 6.4){
+            health = "Prediabetes";
+            symptoms = "Less severe symptoms of diabetes, which include frequent thirst, urination, blurry vision, and constant fatigue"
+    
+        }
+        else {
+            health = Diabetes;
+            symptoms = "Frequent Thirst, Urination, Blurry Vision, Constant Fatigue"
+        }
+}
+function HBA1C(value){
+    var health;  
+    var symptoms;
+        if (value < 5.7) {
+            health = "Normal";
+            symptoms = 
+    
+        }
+        else if (value > 5.7 && value < 6.4){
+            health = "Prediabetes";
+            symptoms = "Less severe symptoms of diabetes, which include frequent thirst, urination, blurry vision, and constant fatigue"
+    
+        }
+        else {
+            health = Diabetes;
+            symptoms = "Frequent Thirst, Urination, Blurry Vision, Constant Fatigue"
+        }
+}
+function HBA1C(value){
+    var health;  
+    var symptoms;
+        if (value < 5.7) {
+            health = "Normal";
+            symptoms = 
+    
+        }
+        else if (value > 5.7 && value < 6.4){
+            health = "Prediabetes";
+            symptoms = "Less severe symptoms of diabetes, which include frequent thirst, urination, blurry vision, and constant fatigue"
+    
+        }
+        else {
+            health = Diabetes;
+            symptoms = "Frequent Thirst, Urination, Blurry Vision, Constant Fatigue"
+        }
+}
+function HBA1C(value){
+    var health;  
+    var symptoms;
+        if (value < 5.7) {
+            health = "Normal";
+            symptoms = 
+    
+        }
+        else if (value > 5.7 && value < 6.4){
+            health = "Prediabetes";
+            symptoms = "Less severe symptoms of diabetes, which include frequent thirst, urination, blurry vision, and constant fatigue"
+    
+        }
+        else {
+            health = Diabetes;
+            symptoms = "Frequent Thirst, Urination, Blurry Vision, Constant Fatigue"
+        }
+}
+function HBA1C(value){
+    var health;  
+    var symptoms;
+        if (value < 5.7) {
+            health = "Normal";
+            symptoms = 
+    
+        }
+        else if (value > 5.7 && value < 6.4){
+            health = "Prediabetes";
+            symptoms = "Less severe symptoms of diabetes, which include frequent thirst, urination, blurry vision, and constant fatigue"
+    
+        }
+        else {
+            health = Diabetes;
+            symptoms = "Frequent Thirst, Urination, Blurry Vision, Constant Fatigue"
+        }
+}
+function HBA1C(value){
+    var health;  
+    var symptoms;
+        if (value < 5.7) {
+            health = "Normal";
+            symptoms = 
+    
+        }
+        else if (value > 5.7 && value < 6.4){
+            health = "Prediabetes";
+            symptoms = "Less severe symptoms of diabetes, which include frequent thirst, urination, blurry vision, and constant fatigue"
+    
+        }
+        else {
+            health = Diabetes;
+            symptoms = "Frequent Thirst, Urination, Blurry Vision, Constant Fatigue"
+        }
+}
+function HBA1C(value){
+    var health;  
+    var symptoms;
+        if (value < 5.7) {
+            health = "Normal";
+            symptoms = 
+    
+        }
+        else if (value > 5.7 && value < 6.4){
+            health = "Prediabetes";
+            symptoms = "Less severe symptoms of diabetes, which include frequent thirst, urination, blurry vision, and constant fatigue"
+    
+        }
+        else {
+            health = Diabetes;
+            symptoms = "Frequent Thirst, Urination, Blurry Vision, Constant Fatigue"
+        }
+}
+function HBA1C(value){
+    var health;  
+    var symptoms;
+        if (value < 5.7) {
+            health = "Normal";
+            symptoms = 
+    
+        }
+        else if (value > 5.7 && value < 6.4){
+            health = "Prediabetes";
+            symptoms = "Less severe symptoms of diabetes, which include frequent thirst, urination, blurry vision, and constant fatigue"
+    
+        }
+        else {
+            health = Diabetes;
+            symptoms = "Frequent Thirst, Urination, Blurry Vision, Constant Fatigue"
+        }
+}
+
+for (let x=0; x < tests.length; x++) {
+    if (test[x] === testNames[0]){
+        HBA1C(intputs[x]);
+    }
+    if (test[x] === testNames[1]){
+        CBC(intputs[x]);
+    }
+    if (test[x] === testNames[2]){
+        CMP(intputs[x]);
+    }
+    if (test[x] === testNames[3]){
+        lipidPanel(intputs[x])
+    }
+    if (test[x] === testNames[4]){
+        prostateSurf(intputs[x]);
+    }
+    if (test[x] === testNames[5]){
+        CA19(intputs[x]);
+    }
+    if (test[x] === testNames[6]){
+        sexually(intputs[x]);
+    }
+    if (test[x] === testNames[7]){
+        urineTox(intputs[x]);
+    }
+    if (test[x] === testNames[8]){
+        urineAl(intputs[x]); 
+    }
+    if (test[x] === testNames[9]){
+        Creact(intputs[x]);
+    }
+    if (test[x] === testNames[10]){
+        liverPanel(intputs[x]);
+    }
+    if (test[x] === testNames[11]){
+        dDimer(intputs[x]);
+    }
+
+       
+
+
+}
